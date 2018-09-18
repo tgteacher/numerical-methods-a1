bc5e54ee13a80d47b15f74a9d5b707911cb9e3df
Tristan Glatard <tristan.glatard@concordia.ca>
2018-09-17 22:44:24 -0400

    Started instructions

--- a1.py
+++ a1.py
@@ -1,8 +1,47 @@
 from numpy import *
 
+
 def example_func():
     '''
-      Example function.
-      The functions implementing A1 should be defined below.
+      Important: READ THIS CAREFULLY. 
+      Task: This function is an example, you don't have to modify it.
+      Example: Nothing to report here, really.
+      Test: This function is is tested in tests/test_example.py
+            This test just gives you a bonus, yay!
+      Hint: The functions below have to be implemented in Python, without
+            using any function from numpy.linalg. In each function, a
+            docstring formatted as the present one explains what the 
+            function must do (Task), gives an example of output 
+            (Example), explains how it will be evaluated (Test), and 
+            may give you some hints (Hint).
     '''
     return 'It works!'
+
+
+def square(a):
+    '''
+      Task: This function must return True if a represents a square matrix.
+      Inputs: a is a numpy array.
+      Example: 
+      Test: This function is is tested in tests/test_example.py
+            This test just gives you a bonus, yay!
+      Hint: The functions below have to be implemented in Python, without
+            using any function from numpy.linalg. In each function, a
+            docstring formatted as the present one explains what the 
+            function must do (Task), gives an example of output 
+            (Example), explains how it will be evaluated (Test), and 
+            may give you some hints (Hint).
+    '''
+
+def fit_poly_2():
+      '''
+      Task: This function is an example, you don't have to modify it.
+      Example: 
+      Test: This function is is tested in tests/test_example.py
+            This test just gives you a bonus, yay!
+      Hint: The functions below have to be implemented in Python, without
+            using any function from numpy.linalg. In each function,
+            docstring formatted as the present one explains 
+            what the function must do (Task), how it will be evaluated (Test),
+            and may give you some hints (Hint).
+    '''
